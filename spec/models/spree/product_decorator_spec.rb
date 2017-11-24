88bc968807044c3f98ae8584070e19a5235fa7d3
Brian Christian <brchristian@gmail.com>
2017-11-24 17:34:35 -0500

    update deprecated finder method in model spec

--- spec/models/spree/product_decorator_spec.rb
+++ spec/models/spree/product_decorator_spec.rb
@@ -6,18 +6,18 @@ RSpec.describe Spree::Product, type: :model do
   describe '.find_by_array_of_ids' do
     it 'returns the products specified in the array of product ids' do
       product_ids = Spree::Product.limit(2).map(&:id)
-      products = Spree::Product.find_by_array_of_ids(product_ids)
+      products = Spree::Product.where(id: product_ids)
       expect(products.size).to be(2)
     end
 
     it 'ignores nonexistant product ids and still return correctly specified products' do
       product_ids = Spree::Product.limit(2).map(&:id) << 200
-      products = Spree::Product.find_by_array_of_ids(product_ids)
+      products = Spree::Product.where(id: product_ids)
       expect(products.size).to be(2)
     end
 
     it 'returns an empty array when no valid ids are specified' do
-      products = Spree::Product.find_by_array_of_ids([200])
+      products = Spree::Product.where(id: [200])
       expect(products).to eq([])
     end
   end
