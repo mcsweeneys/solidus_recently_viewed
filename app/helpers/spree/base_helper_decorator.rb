10f0e43b70e77035f44381eaac1223c695bfc2fa
Brian Christian <brchristian@gmail.com>
2017-11-25 10:43:32 -0800

    follow rubocop style suggestions

--- app/helpers/spree/base_helper_decorator.rb
+++ app/helpers/spree/base_helper_decorator.rb
@@ -1,9 +1,9 @@
 Spree::BaseHelper.module_eval do
-  def get_recently_viewed_products_ids
+  def recently_viewed_products_ids
     (session['recently_viewed_products'] || '').split(', ').map!(&:to_i)
   end
 
-  def get_recently_viewed_products
-    Spree::Product.where(id: get_recently_viewed_products_ids)
+  def recently_viewed_products
+    Spree::Product.where(id: recently_viewed_products_ids)
   end
 end
--- app/models/spree/recently_viewed_configuration.rb
+++ app/models/spree/recently_viewed_configuration.rb
@@ -1,3 +1,5 @@
-class Spree::RecentlyViewedConfiguration < Spree::Preferences::Configuration
-  preference :recently_viewed_products_max_count, :integer, default: 5
+module Spree
+  class RecentlyViewedConfiguration < Spree::Preferences::Configuration
+    preference :recently_viewed_products_max_count, :integer, default: 5
+  end
 end
